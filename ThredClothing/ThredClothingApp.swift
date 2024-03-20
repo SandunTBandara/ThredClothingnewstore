39a69b61ba1ba7f5f41e71aa1ebff36603250513
sandun <devock>
2024-03-21 00:38:00 +0530

    Initial Commit

---
+++ ThredClothing/ThredClothingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThredClothingApp.swift
+//  ThredClothing
+//
+//  Created by Sandun Bandara on 2024-03-21.
+//
+
+import SwiftUI
+
+@main
+struct ThredClothingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
