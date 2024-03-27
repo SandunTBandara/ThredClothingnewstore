26865f52469496c676982f68e8d5a63cc1d56c53
SandunTBandara <devocksandu99@gmail.com>
2024-03-28 00:45:02 +0530

    2024|03|28

---
+++ ThredClothing/View/Login/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  ThredClothing
+//
+//  Created by Sandun Bandara on 2024-03-28.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
